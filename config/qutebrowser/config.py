7b643cac8ee4c8b02be615e6520bd1a455e10683
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2026-04-12 22:44:24 +0600

    noto serif in qutebrowser

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,4 @@
+config.load_autoconfig()
+config.set('fonts.web.family.standard', 'Noto Serif Bengali')
+config.set('fonts.web.family.serif', 'Noto Serif Bengali')
+config.set('fonts.web.family.sans_serif', 'Noto Sans Bengali')
